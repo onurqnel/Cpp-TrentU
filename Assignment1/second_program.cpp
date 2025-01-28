52db83b83a5f804917e93471e4b8519b0646de28
onurqnel <onur.onel2230@gmail.com>
2025-01-27 21:23:03 -0500

    Complete

--- Assignment1/second_program.cpp
+++ Assignment1/second_program.cpp
@@ -1,4 +1,21 @@
 #include <iostream>  // Required for input/output operations
+/*
+ * Onur (Honor) Onel
+ * Student Number: 0865803
+ * Student Email: onuronel@trentu.ca
+ * Date: 2025-01-27
+ *
+ * Program Description:
+ * This program demonstrates a simple swap function that exchanges the values
+ * of two integers using reference variables. The program prompts the user to
+ * input two integers, swaps their values using the swap function, and then
+ * displays the results.
+ *
+ * Data Dictionary:
+ * firstNumber (int): The first number input by the user.
+ * secondNumber (int): The second number input by the user.
+ * temp (int): A temporary variable used in the swap function to store the value of one variable during the swap.
+ */
 
 using namespace std;
 
