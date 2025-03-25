803ad4991e4dc5592e6e0e0249cb0998398dbb14
onurqnel <onur.onel2230@gmail.com>
2025-03-25 15:59:01 -0400

    Completed

--- Templates/main.cpp
+++ Templates/main.cpp
@@ -1,27 +1,85 @@
 #include <iostream>
+using namespace std;
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main()
+template <typename T>
+void swapValues(T& v1, T& v2)
 {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++)
-    {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
-
-    return 0;
+    T temp;
+    temp = v1;
+    v1 = v2;
+    v2 = temp;
 }
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+template <typename T>
+void outputAnyValue(T& value)
+{
+    cout << value << endl;
+}
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    swapValues(a, b);
+    cout << a << endl;
+    cout << b << endl;
+
+    string val1 = "abc";
+    string val2 = "def";
+    swapValues(val1, val2);
+    outputAnyValue(val1);
+    outputAnyValue(val2);
+
+    /*cout << "Please enter a number for the value of x -> 100/x : ";
+    int x = 1;
+    cin >> x;
+    try
+    {
+        if (x == 0)
+        {
+            throw invalid_argument("Cannot divide by zero");
+        }
+        cout << "The new value is -> " << 100 / x << endl;
+    }
+    catch (const invalid_argument& e)
+    {
+        cout << "Exception: " << e.what();
+    }
+
+    try
+    {
+        int age;
+        cout << "Enter your age: ";
+        cin >> age;
+
+        if (cin.fail())
+        {
+            throw invalid_argument("Invalid input. Please enter a numeric value.");
+        }
+
+
+        if (age < 0)
+        {
+            throw out_of_range("Age cannot be negative.");
+        }
+
+        if (age < 18)
+        {
+            throw runtime_error("You must be at least 18 years old.");
+        }
+
+        cout << "You are eligible!" << endl;
+    }
+    catch (const invalid_argument& e)
+    {
+        cout << "Exception: " << e.what();
+    }
+    catch (const out_of_range& e)
+    {
+        cout << "Exception: " << e.what();
+    }
+    catch (const runtime_error& e)
+    {
+        cout << "Exception: " << e.what();
+    }*/
+}
